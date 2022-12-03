4512f1d3e276706945f526e45e89fa49b779f26d
alexunxus <b01401089@ntu.edu.tw>
2022-12-03 16:10:45 -0500

    start simclr training

---
+++ example_scripts/contriever_alex.sh
@@ -0,0 +1,7 @@
+torchrun --standalone --nnodes=1 --nproc_per_node=3 train.py \
+--retriever_model_id bert-base-uncased --pooling average --augmentation delete \
+--prob_augmentation 0.1 --train_data "../contriever/data_scripts/encoded-data/bert-base-uncased/en_XX" \
+--loading_mode split --ratio_min 0.1 --ratio_max 0.5 --chunk_length 256 \
+--momentum 0.9995 --queue_size 60840 --temperature 0.05 --warmup_steps 2000 \
+--total_steps 5000 --lr 0.00005 --scheduler linear --optim adamw --per_gpu_batch_size 32 \
+--output_dir ./checkpoints --torchrun --reset_params --contrastive_mode simclr
--- src/simclr.py
+++ src/simclr.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -54,17 +56,17 @@ class SimClr(nn.Module):
         return self.encoder
 
 
-    def info_nce_loss(self, batch_size: int, n_view: int=2):
-        labels = torch.cat([torch.arange(batch_size) for i in range(2)], dim=0)
+    def info_nce_loss(self, features: torch.tensor, batch_size: int, n_views: int = 2):
+        labels = torch.cat([torch.arange(batch_size) for _ in range(n_views)], dim=0)
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
-        labels = labels.to(self.args.device)
+        labels = labels.to(int(os.environ['LOCAL_RANK']))
 
         features = F.normalize(features, dim=1)
 
         similarity_matrix = torch.matmul(features, features.T)
         
         # discard the main diagonal from both: labels and similarities matrix
-        mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.opt.device)
+        mask = torch.eye(labels.shape[0], dtype=torch.bool).to(int(os.environ['LOCAL_RANK']))
         labels = labels[~mask].view(labels.shape[0], -1)
         similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
         # assert similarity_matrix.shape == labels.shape
@@ -76,16 +78,16 @@ class SimClr(nn.Module):
         negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
 
         logits = torch.cat([positives, negatives], dim=1)
-        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.opt.device)
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(int(os.environ['LOCAL_RANK']))
 
         logits = logits / self.opt.temperature
         return logits, labels
 
     def forward(self, q_tokens, q_mask, k_tokens, k_mask, stats_prefix="", iter_stats={}, **kwargs):
 
-        bsz = len(q_tokens)
+        bsz = len(q_tokens)*torch.cuda.device_count()
         labels = torch.arange(0, bsz, dtype=torch.long, device=q_tokens.device)
-
+        # breakpoint()
         qemb = self.encoder(input_ids=q_tokens, attention_mask=q_mask, normalize=self.norm_query)
         kemb = self.encoder(input_ids=k_tokens, attention_mask=k_mask, normalize=self.norm_doc)
 
@@ -95,7 +97,7 @@ class SimClr(nn.Module):
         gather_qemb = gather_fn(qemb)
 
         logits, labels = self.info_nce_loss(torch.cat([gather_kemb, gather_qemb], dim=0),
-                                            batch_size = bsz, n_view = 2)
+                                            batch_size = bsz, n_views = 2)
         loss = F.cross_entropy(logits, labels, label_smoothing=self.label_smoothing)
 
         # labels = labels + dist_utils.get_rank() * len(kemb)
